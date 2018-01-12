51367ade6dfc566000176ed3bf469256f6fabbf2
AirGuanZ <AirGuanZ@gmail.com>
2018-01-12 19:44:36 +0800

    First upload

---
+++ Source/Main.cpp
@@ -0,0 +1,11 @@
+/*================================================================
+Filename: Main.cpp
+Date: 2018.1.12
+Created by AirGuanZ
+================================================================*/
+#include <iostream>
+
+int main(void)
+{
+    std::cout << "Hello, world!" << std::endl;
+}
