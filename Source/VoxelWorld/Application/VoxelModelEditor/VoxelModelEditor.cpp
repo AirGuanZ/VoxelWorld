f7e89e40c7c4ae63401a605ef84ed507f578a6ca
AirGuanZ <AirGuanZ@gmail.com>
2018-03-09 23:45:29 +0800

    引入命令模式框架

--- Source/VoxelWorld/Application/VoxelModelEditor/VoxelModelEditor.cpp
+++ Source/VoxelWorld/Application/VoxelModelEditor/VoxelModelEditor.cpp
@@ -3,9 +3,76 @@ Filename: VoxelModelEditor.cpp
 Date: 2018.3.5
 Created by AirGuanZ
 ================================================================*/
+#include <cassert>
+#include <map>
+
+#include <Utility/HelperFunctions.h>
+
+#include <Resource/ResourceNameManager.h>
 #include "VoxelModelEditor.h"
+
+namespace
+{
+    class VMECmd_ExitClicked : public VoxelModelEditorCommand
+    {
+    public:
+        void Execute(VoxelModelEditorCore &core)
+        {
+            core.mainLoopDone_ = true;
+        }
+    };
+
+    class VMECmd_ReloadBindingNames : public VoxelModelEditorCommand
+    {
+    public:
+        void Execute(VoxelModelEditorCore &core)
+        {
+            core.bindingNames_.clear();
+            core.selectedBindingNameIndex_ = 0;
+
+            auto &rM = RscNameMgr::GetInstance();
+            std::map<std::string, std::wstring> filenames;
+            Helper::GetFilenames(
+                rM.AsString("VoxelModelEditor", "Binding"),
+                rM.AsString("VoxelModelEditor", "BindingExtension"),
+                filenames);
+            for(auto &it : filenames)
+                core.bindingNames_.push_back(it.first);
+
+            core.needRefreshDisplay_ = true;
+        }
+    };
+}
+
+bool VoxelModelEditorCore::Initialize(void)
+{
+    mainLoopDone_ = false;
+    needRefreshDisplay_ = false;
+
+    return true;
+}
 
 AppState VoxelModelEditor::Run(void)
 {
+    if(!VoxelModelEditorCore::Initialize())
+        return AppState::MainMenu;
+
+    cmdQueue_.push(new VMECmd_ReloadBindingNames());
+    while(!mainLoopDone_)
+    {
+        view_.Render();
+
+        while(cmdQueue_.size())
+        {
+            auto *cmd = cmdQueue_.front();
+            cmdQueue_.pop();
+            cmd->Execute(*this);
+            Helper::SafeDeleteObjects(cmd);
+        }
+
+        if(needRefreshDisplay_)
+            RefreshDisplay();
+    }
+
     return AppState::MainMenu;
 }
--- Source/VoxelWorld/Application/VoxelModelEditor/VoxelModelEditor.h
+++ Source/VoxelWorld/Application/VoxelModelEditor/VoxelModelEditor.h
@@ -5,10 +5,49 @@ Created by AirGuanZ
 ================================================================*/
 #pragma once
 
+#include <queue>
+#include <string>
+#include <vector>
+
 #include <Application/Common.h>
 
-class VoxelModelEditor
+class VoxelModelEditorCore
+{
+public:
+    bool Initialize(void);
+
+    int selectedBindingNameIndex_;
+    std::vector<std::string> bindingNames_;
+
+    bool mainLoopDone_;
+    bool needRefreshDisplay_;
+};
+
+class VoxelModelEditorCommand
+{
+public:
+    virtual void Execute(VoxelModelEditorCore &core) = 0;
+};
+
+class VoxelModelEditorView
+{
+public:
+    void Render(void);
+
+private:
+    std::vector<const char*> bindingNames_;
+};
+
+class VoxelModelEditor : private VoxelModelEditorCore
 {
 public:
     AppState Run(void);
+
+private:
+    void RefreshDisplay(void);
+
+private:
+    VoxelModelEditorView view_;
+
+    std::queue<VoxelModelEditorCommand*> cmdQueue_;
 };
