edd8194c0fedefc12eb6ec80cffabaed024ea32c
AirGuanZ <AirGuanZ@gmail.com>
2018-03-20 21:02:12 +0800

    将VMEComponent网格模型修改为以singleBufferMesh实现

--- Source/VoxelWorld/Application/VoxelModelEditor/VoxelModelEditorComponentView.cpp
+++ Source/VoxelWorld/Application/VoxelModelEditor/VoxelModelEditorComponentView.cpp
@@ -5,28 +5,25 @@ Created by AirGuanZ
 ================================================================*/
 #include <cassert>
 
+#include <Utility\HelperFunctions.h>
+
 #include "VoxelModelEditorComponentView.h"
 
-InputLayout VMEComponentMesh::CreateInputLayout(const void *shaderByteCode, int length)
+InputLayout VMEComponentVertex::CreateInputLayout(const void *shaderByteCode, int length)
 {
     assert(shaderByteCode != nullptr && length > 0);
 
     const D3D11_INPUT_ELEMENT_DESC desc[] =
     {
         { "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0,
-            Helper::MemOffset(&Vertex::pos), D3D11_INPUT_PER_VERTEX_DATA, 0 },
+            Helper::MemOffset(&VMEComponentVertex::pos), D3D11_INPUT_PER_VERTEX_DATA, 0 },
         { "NORMAL", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0,
-            Helper::MemOffset(&Vertex::nor), D3D11_INPUT_PER_VERTEX_DATA, 0 },
+            Helper::MemOffset(&VMEComponentVertex::nor), D3D11_INPUT_PER_VERTEX_DATA, 0 },
         { "COLOR", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0,
-            Helper::MemOffset(&Vertex::color), D3D11_INPUT_PER_VERTEX_DATA, 0 }
+            Helper::MemOffset(&VMEComponentVertex::color), D3D11_INPUT_PER_VERTEX_DATA, 0 }
     };
     InputLayout rt;
     if(!rt.Initialize(desc, shaderByteCode, static_cast<UINT>(length)))
         return InputLayout();
     return rt;
-}
-
-VMEComponentMesh::VMEComponentMesh(void)
-{
-    idxCount_ = 0;
 }
--- Source/VoxelWorld/Application/VoxelModelEditor/VoxelModelEditorComponentView.h
+++ Source/VoxelWorld/Application/VoxelModelEditor/VoxelModelEditorComponentView.h
@@ -7,38 +7,19 @@ Created by AirGuanZ
 
 #include <vector>
 
-#include <Utility\D3D11Header.h>
 #include <Utility\Math.h>
 
-#include <D3DObject\BasicBuffer.h>
 #include <D3DObject\InputLayout.h>
-#include <D3DObject\FrameBuffer.h>
+#include <D3DObject\SingleBufferMesh.h>
 #include "VoxelModelEditorCore.h"
 
-class VMEComponentMesh
+struct VMEComponentVertex
 {
-public:
-    struct Vertex
-    {
-        Vector3 pos;    // POSITION
-        Vector3 nor;    // NORMAL
-        Vector3 color;  // COLOR
-    };
-
     static InputLayout CreateInputLayout(const void *shaderByteCode, int length);
 
-    VMEComponentMesh(void);
-
-    bool Initialize(const Vertex *vtxData, int vtxCount, const UINT16 *idxData, int idxCount);
-    bool IsAvailable(void) const;
-    void Destroy(void);
-
-    void Bind(ID3D11DeviceContext *DC);
-    void Unbind(ID3D11DeviceContext *DC);
-    void Draw(void);
-
-private:
-    VertexBuffer<false> vtxBuf_;
-    IndexBuffer<false> idxBuf_;
-    int idxCount_;
+    Vector3 pos;    // POSITION
+    Vector3 nor;    // NORMAL
+    Vector3 color;  // COLOR
 };
+
+using VMEComponentMesh = SingleBufferMesh<VMEComponentVertex>;
